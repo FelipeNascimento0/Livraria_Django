4ceabe2f506f030862749756e688f4d3294c633f
Felipe Kowalski <felipenascimentoo414@Gmail.com>
2022-02-27 22:20:47 -0300

    Adição das Classes de Autor e Livro

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora
+from .models import Autor , Categoria, Editora, Livro
 
 
 class EditoraAdmin (admin.ModelAdmin):
     list_display = ("nome","site")
 
+class LivroAdmin (admin.ModelAdmin):
+    list_display = ("titulo","ISBN","quantidade","preco","categoria","editora")
+
+
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora,EditoraAdmin)
+admin.site.register(Livro,LivroAdmin)
--- core/models.py
+++ core/models.py
@@ -3,6 +3,15 @@ from django.db import models
 
 # Create your models here.
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255, unique=TRUE, verbose_name="descrição")
 
@@ -15,3 +24,14 @@ class Editora(models.Model):
 
     def __str__(self):
         return "{} - ({})".format(self.nome, self.site)
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.FloatField(verbose_name="preço")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+
+    def __str__(self):
+        return self.titulo
